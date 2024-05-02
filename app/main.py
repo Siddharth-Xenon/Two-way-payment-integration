380c00bd7b45291bafa63fe81663d8e50747b607
Siddharth <sidsolanki920@gmail.com>
2024-05-02 20:55:32 +0530

    Refactor table creation code in main.py

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 import uvicorn
 from fastapi import FastAPI
+
 from app.api.api import api_router
 from app.db.db import engine
 from app.models import customers
@@ -9,7 +10,9 @@ app = FastAPI(title="Zenskar", description="Two-Way Integrations")
 
 
 def create_tables():
-    print("Creating tables")
+    """
+    This can futher moved to a new initialization file, if we have more tables to create
+    """
     customers.Base.metadata.create_all(bind=engine)
 
 
@@ -19,16 +22,6 @@ app.include_router(api_router)
 @app.on_event("startup")
 async def startup_event():
     create_tables()
-
-
-# @app.middleware("http")
-# async def add_process_time_header(request: Request, call_next):
-#     """
-#     A middleware that handles the lifespan events.
-#     This approach aligns with the latest FastAPI practices as suggested in the documentation.
-#     """
-#     response = await call_next(request)
-#     return response
 
 
 @app.get("/")
