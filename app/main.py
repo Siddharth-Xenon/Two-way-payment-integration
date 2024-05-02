2752cfdd9003aba2e2c88f5b25cc68c3ad2bd222
Siddharth <sidsolanki920@gmail.com>
2024-05-02 10:50:42 +0530

    Add database connection and table creation

--- app/main.py
+++ app/main.py
@@ -1,11 +1,34 @@
 import uvicorn
 from fastapi import FastAPI
 from app.api.api import api_router
+from app.db.db import engine
+from app.models import customers
+
 
 app = FastAPI(title="Zenskar", description="Two-Way Integrations")
 
 
+def create_tables():
+    print("Creating tables")
+    customers.Base.metadata.create_all(bind=engine)
+
+
 app.include_router(api_router)
+
+
+@app.on_event("startup")
+async def startup_event():
+    create_tables()
+
+
+# @app.middleware("http")
+# async def add_process_time_header(request: Request, call_next):
+#     """
+#     A middleware that handles the lifespan events.
+#     This approach aligns with the latest FastAPI practices as suggested in the documentation.
+#     """
+#     response = await call_next(request)
+#     return response
 
 
 @app.get("/")
