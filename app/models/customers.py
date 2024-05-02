49711ef5e14d3c64bb2d9aaae9c5be4879030b86
Siddharth <sidsolanki920@gmail.com>
2024-05-02 20:55:43 +0530

    Refactor Customer model and add BaseModel

--- app/models/customers.py
+++ app/models/customers.py
@@ -1,10 +1,20 @@
-from sqlalchemy import Column, Integer, String
+import uuid
+from pydantic import BaseModel
+from sqlalchemy import Column, String
 from app.db.db import Base
 
 
-class Customer(Base):
+class Customer(BaseModel):
+    name: str
+    email: str
+
+    class Config:
+        orm_mode = True
+
+
+class CustomerDB(Base):
     __tablename__ = "customers"
 
-    id = Column(Integer, primary_key=True, index=True)
+    id = Column(String, primary_key=True, default=lambda: uuid.uuid4().hex, index=True)
     name = Column(String, index=True)
     email = Column(String, unique=True, index=True)
